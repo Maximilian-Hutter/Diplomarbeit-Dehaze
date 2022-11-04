ba0cc308b7ecc2fb3f7234a44477de92416a43a7
Maximilian Hutter <redmiralis@gmail.com>
2022-11-04 09:02:52 +0100

    changed the target directory

--- data.py
+++ data.py
@@ -10,15 +10,16 @@ import myutils
 class ImageDataset(Dataset):
     def __init__(self, root, size,crop_size,augmentation=0):
 
+        self.root = root
         self.augmentation = augmentation
         self.crop_size = crop_size
         self.size = size
-        self.gt = sorted(os.listdir(root + "/gt/*.*"))
-        self.imgs = sorted(os.listdir(root + "/input/*.*"))
+        self.gt = sorted(os.listdir(root + "/gt"))
+        self.imgs = sorted(os.listdir(root + "/input"))
 
     def __getitem__(self, index):   # get images to dataloader
 
-        label = Image.open(self.gt[index % len(self.imgs)])
+        label = Image.open(self.root + self.gt[index % len(self.imgs)])
         if self.crop_size != None:
             label = myutils.crop_center(label, self.crop_size, self.crop_size)  # crop image if the images are not the same size
         label = label.resize(size = self.size)  # change size
