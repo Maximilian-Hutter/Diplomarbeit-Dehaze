01a80e551f70ed6c2432d7be66a6560d33b2dc2e
Maximilian Hutter <redmiralis@gmail.com>
2022-11-14 08:52:32 +0100

    added DensityEstimation

--- models.py
+++ models.py
@@ -17,14 +17,15 @@ class CoT(nn.Module): # https://arxiv.org/pdf/2107.12292v1.pdf change BN to IN
 class TailModule(nn.Module):
     def __init__(self, in_feat, out_feat, kernel, padw, padh):
         super(TailModule, self).__init__()
-        self.pad = (padw,padw,padh,padh)
-        self.dlkcb = DLKCB(in_feat, out_feat, kernel)
+        pad = (padw,padw,padh,padh)
+        self.padding = nn.ReflectionPad2d(pad)
+        self.dlkcb = ConvBlock(in_feat, out_feat, kernel)
         self.elu = nn.ELU()
         self.conv2 = ConvBlock(out_feat, out_feat, 3)
         self.tanh = nn.Tanh()
     
     def forward(self,x):
-        x = F.pad(x,self.pad, "constant")
+        x = self.padding(x)
         x = self.dlkcb(x)
         x = self.elu(x)
         x = self.conv2(x)
@@ -74,16 +75,16 @@ class MHAC(nn.Module):
         return out
         
 class SHA(nn.Module):
-    def __init__(self, in_feat, out_feat, groups):
+    def __init__(self, in_feat, out_feat, groups, kernel = 3):
         super(SHA,self).__init__()
         self.groups = groups
 
         # might be wrong
-        self.avgh = nn.AvgPool2d((in_feat,1),stride=1) # kernel of size 1 horizontaly and 0 verticaly
-        self.maxh = nn.MaxPool2d((in_feat,1),stride=1)
+        self.avgh = nn.AvgPool2d((kernel,1),stride=1) # kernel of size 1 horizontaly and 0 verticaly
+        self.maxh = nn.MaxPool2d((kernel,1),stride=1)
 
-        self.avgv = nn.AvgPool2d((1,in_feat),stride=1)
-        self.maxv = nn.MaxPool2d((1,in_feat),stride=1)
+        self.avgv = nn.AvgPool2d((1,kernel),stride=1)
+        self.maxv = nn.MaxPool2d((1,kernel),stride=1)
         #
 
         self.shuffle = nn.ChannelShuffle(groups)
@@ -97,6 +98,7 @@ class SHA(nn.Module):
 
     def forward(self,x):
 
+        print(x.shape)
         res = x
         havg = self.avgh(x)
         hmax = self.maxh(x)
@@ -110,6 +112,8 @@ class SHA(nn.Module):
 
         h = F.pad(h, (0,0,1,1), "constant",0)
         v = F.pad(v, (1,1), "constant",0)
+        print(h.shape)
+        print(v.shape)
         x = torch.cat((h,v))
         # put x to cpu because channel_shuffle no cuda backend
         x = x.to("cpu")
@@ -158,10 +162,26 @@ class AdaptiveFeatureFusion(nn.Module):
         return out
 
 class DensityEstimation(nn.Module):
-    def __init__(self):
+    def __init__(self, in_feat, kernel, groups, padw, padh, first_conv_feat = 64):
         super(DensityEstimation, self).__init__()
 
+        # path # conv -> reflective pad, -> sha -> conv -> sigmoid
+        self.dlkcb = DLKCB(in_feat, first_conv_feat, kernel)
+        self.pad = nn.ReflectionPad2d((padw,padw,padh,padh))
+        self.sha = SHA(first_conv_feat, first_conv_feat, groups)
+        self.conv = ConvBlock(first_conv_feat, 1, 3)
+        self.sigmoid = nn.Sigmoid()
+
     def forward(self,x):
+        res = x
+        x = self.dlkcb(x)
+        x = self.pad(x)
+        x = self.sha(x)
+        x = self.conv(x)
+        out = self.sigmoid(x)
+
+        out = torch.mul(out, res)
+
         return out
 
 class Shallow(nn.Module):
